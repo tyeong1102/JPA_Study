79dd6e0655395c97d1cd092cf412d1dc398213d1
Kim Taeyeong <tyeong1102@gmail.com>
2023-08-04 22:27:37 +0900

    프로젝트 환경설정

---
+++ SpringBoot JPA - web application/jpashops/src/main/java/jpabook/jpashops/JpashopsApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopsApplication.class, args);
+	}
+
+}
---
+++ SpringBoot JPA - web application/jpashops/src/test/java/jpabook/jpashops/JpashopsApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpashops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
