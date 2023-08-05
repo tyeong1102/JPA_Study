b4b536070fc8d821aea6a4daba64bafd4a325a7d
Kim Taeyeong <tyeong1102@gmail.com>
2023-08-05 19:51:11 +0900

    엔티티 클래스 개발

---
+++ SpringBoot JPA - web application/jpashops/src/main/java/jpabook/jpashops/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package jpabook.jpashops.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ SpringBoot JPA - web application/jpashops/src/main/java/jpabook/jpashops/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package jpabook.jpashops.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
