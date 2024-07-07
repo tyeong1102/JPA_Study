2decc9971b25dce7795b4a937d06ac84fd3a1430
Kim Taeyeong <tyeong1102@gmail.com>
2024-07-07 23:43:26 +0900

    영속성 관리 - 내부 동작 방식

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -8,20 +8,22 @@ public class JpaMain {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
         EntityManager em = emf.createEntityManager();
+
         EntityTransaction tx = em.getTransaction();
         
-        tx.begin();
+//        tx.begin();
 
         try {
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
 
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+            // 영속
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
+
+            em.detach(member);
+
+            System.out.println("==================");
 
             tx.commit();
         } catch(Exception e) {
