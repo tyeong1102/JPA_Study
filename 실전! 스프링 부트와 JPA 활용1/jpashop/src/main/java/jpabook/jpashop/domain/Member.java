f4be041ba83c84594631508d1364de4dedbabef8
Kim Taeyeong <tyeong1102@gmail.com>
2024-07-21 21:08:17 +0900

    도메인 분석 설계

---
+++ 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded // 내장 타입인지 아닌지 판단
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
