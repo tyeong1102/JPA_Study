07ac509dee9edf8682ea42964d6d65b5352cacc6
Kim Taeyeong <tyeong1102@gmail.com>
2024-07-24 20:27:41 +0900

    웹 계층 개발

--- 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -60,9 +60,9 @@ public class OrderService {
         order.cancel();
     }
 
-//    //검색
-//    public List<Order> findOrders(OrderSearch orderSearch) {
-//        return orderRepository.findAll(orderSearch);
-//    }
+    //검색
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
+    }
 
 }
